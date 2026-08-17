f0a408b9fc481b49d5178a7dabfbf4289025ca20
samia313 <samiaahmadnaveed@gmail.com>
2026-08-18 02:32:53 +0500

    fix: stop homepage social metadata leaking into tool pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,15 +30,11 @@ export const metadata: Metadata = {
   openGraph: {
     type: 'website',
     locale: 'en_US',
-    title: SITE_TITLE,
-    description: SITE_DESCRIPTION,
     siteName: 'PDFilio',
     images: [{ url: '/og-image.png', width: 1200, height: 630, alt: 'PDFilio - Free PDF Tools' }],
   },
   twitter: {
     card: 'summary_large_image',
-    title: SITE_TITLE,
-    description: SITE_DESCRIPTION,
     images: ['/og-image.png'],
   },
   robots: {
@@ -69,7 +65,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
       <head suppressHydrationWarning>
         <script
           dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-T9N4TQVD');`,
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+i:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-T9N4TQVD');`,
           }}
         />
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3342033551482593" crossOrigin="anonymous" />
