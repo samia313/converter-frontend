6b0e5c7e4b4752e489c10761187eb5bbd508d6f5
samia313 <samiaahmadnaveed@gmail.com>
2026-09-20 00:16:03 +0500

    Add HTML to PDF LibreOffice converter

--- digitalocean-server/converters/index.js
+++ digitalocean-server/converters/index.js
@@ -80,7 +80,8 @@ async function libreOfficeConvert(inputPath, outputPath, format) {
 const pdfToWord = (inputPath, outputPath) => libreOfficeConvert(inputPath, outputPath, 'docx')
 const pdfToExcel = (inputPath, outputPath) => libreOfficeConvert(inputPath, outputPath, 'xlsx')
 const pdfToPowerPoint = (inputPath, outputPath) => libreOfficeConvert(inputPath, outputPath, 'pptx')
-const powerpointToPdf = (inputPath, outputPath) => libreOfficeConvert(inputPath, outputPath, 'pdf:impress_pdf_Export')
+const powerpointToPdfconst htmlToPdf = (inputPath, outputPath) => libreOfficeConvert(inputPath, outputPath, 'pdf:writer_pdf_Export')
+ = (inputPath, outputPath) => libreOfficeConvert(inputPath, outputPath, 'pdf:impress_pdf_Export')
 
 async function pdfToImages(inputPath, outputDir) {
   return withConversionSlot(async () => {
@@ -112,4 +113,4 @@ async function pdfOCR(inputPath, outputPath, language = 'eng') {
   })
 }
 
-module.exports = { pdfToWord, pdfToExcel, pdfToPowerPoint, powerpointToPdf, pdfToImages, pdfOCR }
+module.exports = { pdfToWord, pdfToExcel, pdfToPowerPoint, powerpointToPdf, htmlToPdf, pdfToImages, pdfOCR }
